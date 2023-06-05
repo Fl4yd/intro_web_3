eeae82e796ecc56db21d14c1f2d54288e4b42385
Jere Arokivi <51503069+Fl4yd@users.noreply.github.com>
2023-06-05 08:58:22 +0000

    Tehtava 5 asti

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ async function getData() {
 }
 
 import data from "./dataset.json";
+import data2 from "./dataset2.json";
 
 function range(start, end) {
   if(start === end) return [start];
@@ -28,24 +29,42 @@ for(var key in labels) {
 }
 
 const values = data.dataset.value;
+const values2 = data2.dataset.value;
 const table_body = document.getElementById('table-body');
 const table_head = document.getElementById('table-head')
-let tr = document.createElement('tr')
-let th1 = document.createElement('th')
-let th2 = document.createElement('th')
+let tr = document.createElement('tr');
+let th1 = document.createElement('th');
+let th2 = document.createElement('th');
+let th3 = document.createElement('th');
+let th4 = document.createElement('th')
 th1.innerText = "Municipality";
 th2.innerText = "Population";
+th3.innerText = "Employment amount";
+th4.innerText = "Employment-%";
 tr.appendChild(th1);
 tr.appendChild(th2);
+tr.appendChild(th3);
+tr.appendChild(th4);
 table_head.appendChild(tr);
 
 for(var idx in range(0,309)) {
   let tr = document.createElement('tr');
   let td1 = document.createElement('td');
   let td2 = document.createElement('td');
+  let td3 = document.createElement('td');
+  let td4 = document.createElement('td')
   td1.innerText = label_strings[idx];
   td2.innerText = values[idx];
+  td3.innerText = values2[idx];
+  td4.innerText = Math.round(values2[idx]/values[idx] * 10000)/100 + "%"
+  if (Math.round(values2[idx]/values[idx] * 10000)/100 > 45) {
+    tr.setAttribute('id','emp1')
+  }else if (Math.round(values2[idx]/values[idx] * 10000)/100 < 25) {
+    tr.setAttribute('id','emp2')
+  }
   tr.appendChild(td1);
   tr.appendChild(td2);
+  tr.appendChild(td3);
+  tr.appendChild(td4);
   table_body.appendChild(tr);
 }
